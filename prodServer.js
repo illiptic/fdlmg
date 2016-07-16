175cb74171985cdb905e168a2dfa45b5dd709cb2
illiptic <jeremy.davis42@gmail.com>
2016-07-16 11:35:46 +0200

    Fix prod server

--- prodServer.js
+++ prodServer.js
@@ -1,17 +1,19 @@
-var path = require('path');
-var express = require('express');
+var path = require('path')
+var express = require('express')
 
-var app = express();
+var app = express()
+
+app.use('/static', express.static(path.join(__dirname, 'dist')))
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
-});
+  res.sendFile(path.join(__dirname, 'src/index.html'))
+})
 
-app.listen(80, 'localhost', function(err) {
+app.listen(8080, 'localhost', function(err) {
   if (err) {
-    console.log(err);
+    console.log(err)
     return;
   }
 
-  console.log('Listening at http://localhost:80');
-});
+  console.log('Listening at http://localhost:8080')
+})
