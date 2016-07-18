38d3317b19ad9cd3ffb2489f266dcc6f29e97bc3
illiptic <jeremy.davis42@gmail.com>
2016-07-18 20:27:38 +0200

    Fix prod build

--- prodServer.js
+++ prodServer.js
@@ -3,17 +3,20 @@ var express = require('express')
 
 var app = express()
 
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.use('/static', express.static(path.join(__dirname, 'dist')))
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'src/index.html'))
 })
 
-app.listen(8080, 'localhost', function(err) {
+app.listen(port, ip_address, function(err) {
   if (err) {
     console.log(err)
     return;
   }
 
-  console.log('Listening at http://localhost:8080')
+  console.log('Listening at http://' + ip_address + ':' + port)
 })
